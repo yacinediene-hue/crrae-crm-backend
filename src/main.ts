a685e4cced149c0883bbfe2a66dac71112bb083d
Yacine diene <yacinediene@192.168.1.13>
2026-03-16 15:59:55 +0100

    fix railway binding

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`CRRAE-CRM Backend running on port ${port}`);
 }
 
